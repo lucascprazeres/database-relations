26680752dc80416e6b97643d0e4246ad1959e1aa
lucascprazeres <olucascostadp@gmail.com>
2020-08-01 19:12:06 -0300

    implementa crição de usuários

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,21 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const foundCostumer = await this.customersRepository.findByEmail(email);
+
+    if (foundCostumer) {
+      throw new AppError('This email is already in use.');
+    }
+
+    const costumer = await this.customersRepository.create({ name, email });
+
+    return costumer;
   }
 }
 
